8fea9f0cb5e0df985beb74b1a9d30f59acd97fab
ribhavsingla <ribhavsingla2166@gmail.com>
2025-03-26 12:17:53 +0530

    fixed the next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://cpu-scheduling-visualizer-ribhav.vercel.app/', 
-    generateRobotsTxt: true,
-    changefreq: 'never',
-    priority: 0.7,
-    sitemapSize: 5000, 
-    exclude: [], 
-  };
-  
+  siteUrl: 'https://cpu-scheduling-visualizer-ribhav.vercel.app/', 
+  generateRobotsTxt: true,
+  changefreq: 'never',
+  priority: 0.7,
+  sitemapSize: 5000, 
+  exclude: [],
+  robotsTxtOptions: {
+      policies: [
+          { userAgent: '*', allow: '/' }
+      ],
+  },
+};
